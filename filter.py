0de0a079900547411657131910436c8ef92393c0
JudeAlexis111 <60825550+JudeAlexis111@users.noreply.github.com>
2023-11-20 16:56:13 -0800

    inital commit

---
+++ filter.py
@@ -0,0 +1,37 @@
+import json
+import requests
+import re
+import time
+from bs4 import BeautifulSoup
+
+file_path = 'combinedJournalList.json'
+journalArray = []
+validJournals = []
+totalArticles = 0
+totalJournals = 0
+
+with open(file_path, 'r') as file:
+    journalArray = json.load(file)
+
+for x in journalArray:
+    url = f"https://api.crossref.org/journals/{x}"
+    rquest = requests.get(url)
+
+    if rquest.status_code == 200:
+        request = rquest.json()
+        current_dois = request['message']['counts']['current-dois']
+
+        totalArticles += int(current_dois)
+        totalJournals += 1
+
+        validJournals.append(x)
+
+        print(current_dois)
+
+metrics = [totalArticles, totalJournals]
+
+with open('finalJournalList.json', 'w') as json_file:
+    json.dump(validJournals, json_file)
+
+with open('finalJournalMetrics.json', 'w') as json_file:
+    json.dump(metrics, json_file)
